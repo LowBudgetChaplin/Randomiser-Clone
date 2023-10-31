c5e8f46c37211712f46612565c839788e82f4fba
Serban Corodescu <103532635+LowBudgetChaplin@users.noreply.github.com>
2023-10-31 02:29:56 +0200

    Delete app2.js

--- app2.js
+++
@@ -1,6 +0,0 @@
-
-function chooseRandom(){
-       let words = document.getElementById("textarea").value.split(" ");
-       document.getElementById("result").value =
-       words[Math.floor(Math.random() * words.length)];
-}
