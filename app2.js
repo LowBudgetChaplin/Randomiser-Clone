38550494f51a565260f2dd2f73a9127f69f9b157
Serban Corodescu <serbancorodescu14@gmail.com>
2023-10-31 02:17:48 +0200

    random small app commit

---
+++ app2.js
@@ -0,0 +1,6 @@
+
+function chooseRandom(){
+       let words = document.getElementById("textarea").value.split(" ");
+       document.getElementById("result").value =
+       words[Math.floor(Math.random() * words.length)];
+}
